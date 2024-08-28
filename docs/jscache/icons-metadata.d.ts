af8fda6c01252524e00deb60c2827dcc8c05d67d
dbetka <kontakt@dbetka.pl>
2024-08-28 10:26:42 +0200

    Update docs page

---
+++ docs/jscache/icons-metadata.d.ts
@@ -0,0 +1,10 @@
+export declare const iconsMetadata: {
+    name: string;
+    version: number;
+    popularity: number;
+    codepoint: number;
+    unsupported_families: string[];
+    categories: string[];
+    tags: string[];
+    sizes_px: number[];
+}[];
